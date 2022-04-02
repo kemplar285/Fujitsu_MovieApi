128039e0de940a29bb18a10a82dc318d56beddb4
e8e4caivtw <kemplar285@gmail.com>
2022-04-02 13:43:51 +0300

    Blank project

---
+++ src/main/java/ee/fujitsu/movieapi/JavaMovieApiApplication.java
@@ -0,0 +1,13 @@
+package ee.fujitsu.movieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/fujitsu/movieapi/JavaMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.fujitsu.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
